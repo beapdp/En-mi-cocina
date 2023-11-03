436ce6f3933515daf2f06d35048706e972123c06
beapdp <bea.palomo.delpozo@gmail.com>
2023-11-03 22:03:20 +0100

    mejoras en el código del componente Menu

--- En-mi-cocina/en-mi-cocina/src/components/Menu.js
+++ En-mi-cocina/en-mi-cocina/src/components/Menu.js
@@ -1,12 +1,13 @@
-import React, { useState } from 'react';
+// Importamos los hooks y las dependencias necesarias
+import React, { useState, useCallback } from 'react';
 import '../styles/menu.css'
 
-// Este componente será el Menú de la semana.
+// Definimos el componente Menu
 export const Menu = () => {
-  // Este array representa los días de la semana.
+  // Array con los días de la semana
   const daysOfWeek = ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo'];
 
-  // Este estado guarda las comidas y cenas para cada día de la semana. Comenzamos con todas las comidas y cenas como nulas.
+  // Estado que guarda las comidas y cenas para cada día de la semana
   const [meals, setMeals] = useState({
     'Lunes': { lunch: null, dinner: null },
     'Martes': { lunch: null, dinner: null },
@@ -17,7 +18,7 @@ export const Menu = () => {
     'Domingo': { lunch: null, dinner: null },
   });
 
-  // Este estado controla si el input de texto debe mostrarse o no para cada comida y cena. Comienza como verdadero para todos.
+  // Estado que controla si el input de texto debe mostrarse o no para cada comida y cena
   const [inputVisible, setInputVisible] = useState({
     'Lunes': { lunch: true, dinner: true },
     'Martes': { lunch: true, dinner: true },
@@ -28,84 +29,89 @@ export const Menu = () => {
     'Domingo': { lunch: true, dinner: true },
   });
 
-  // Esta función se llama cuando se quiere agregar una comida o cena a un día.
-  const addMeal = (day, mealType, meal) => {
-    // Si la comida no está vacía, la añadimos al estado de las comidas.
+  // Función para agregar una comida o cena a un día, usando useCallback para evitar recreaciones innecesarias de la función
+  const addMeal = useCallback((day, mealType, meal) => {
+    // Verificamos que la comida no esté vacía antes de agregarla
     if (meal !== "") {
-      setMeals((prevMeals) => {
-        return { ...prevMeals, [day]: { ...prevMeals[day], [mealType]: meal }};
-      });
-      // Ocultamos el input después de añadir la comida.
-      setInputVisible(prevState => {
-        return { ...prevState, [day]: { ...prevState[day], [mealType]: false }};
-      });
+      // Actualizamos el estado de las comidas
+      setMeals(prevMeals => ({
+        ...prevMeals,
+        [day]: { ...prevMeals[day], [mealType]: meal },
+      }));
+      // Ocultamos el input después de añadir la comida
+      setInputVisible(prevState => ({
+        ...prevState,
+        [day]: { ...prevState[day], [mealType]: false },
+      }));
     }
-  };
+  }, []);
 
-  // Esta función se llama cuando se quiere eliminar una comida o cena de un día.
-  const removeMeal = (day, mealType) => {
-    // Eliminamos la comida del estado de las comidas.
-    setMeals(prevMeals => {
-      return { ...prevMeals, [day]: { ...prevMeals[day], [mealType]: null }};
-    });
-    // Mostramos el input después de eliminar la comida.
-    setInputVisible(prevState => {
-      return { ...prevState, [day]: { ...prevState[day], [mealType]: true }};
-    });
-  }
+  // Función para eliminar una comida o cena de un día, usando useCallback para evitar recreaciones innecesarias de la función
+  const removeMeal = useCallback((day, mealType) => {
+    // Eliminamos la comida del estado de las comidas
+    setMeals(prevMeals => ({
+      ...prevMeals,
+      [day]: { ...prevMeals[day], [mealType]: null },
+    }));
+    // Mostramos el input después de eliminar la comida
+    setInputVisible(prevState => ({
+      ...prevState,
+      [day]: { ...prevState[day], [mealType]: true },
+    }));
+  }, []);
 
-  // Esta es la función de renderizado del componente.
+  // Renderizamos el componente
   return (
     <div className="menu">
-      {/* Iteramos sobre los días de la semana. */}
-      {daysOfWeek.map((day) => (
-        <div key={day} className="day">
-          <h3 className="day-name">{day}</h3>
-          <div className="meal">
-            <h4>Comida</h4>
-            {/* Si el input para la comida de este día debe ser visible, lo mostramos. */}
-            {inputVisible[day].lunch ? (
-              <input
-                type="text"
-                placeholder="Añadir comida"
-                onBlur={(e) => addMeal(day, 'lunch', e.target.value)}
-              />
-            ) : (
-              /* Si no, mostramos la comida y un checkbox para eliminarla. */
-              <div>
-                <p>{meals[day].lunch}</p>
-                <input 
-                  type="checkbox" 
-                  checked={!inputVisible[day].lunch}
-                  onChange={() => removeMeal(day, 'lunch')}
+      {daysOfWeek.map((day) => {
+        // Desestructuramos lunch y dinner de meals[day] y inputVisible[day] para mejorar la legibilidad
+        const { lunch, dinner } = meals[day];
+        const { lunch: lunchVisible, dinner: dinnerVisible } = inputVisible[day];
+
+        return (
+          <div key={day} className="day">
+            <h3 className="day-name">{day}</h3>
+            <div className="meal">
+              <h4>Comida</h4>
+              {lunchVisible ? (
+                <input
+                  type="text"
+                  placeholder="Añadir comida"
+                  onBlur={(e) => addMeal(day, 'lunch', e.target.value)}
                 />
-              </div>
-            )}
-          </div>
-          <div className="meal">
-            <h4>Cena</h4>
-            {/* Lo mismo para la cena. */}
-            {inputVisible[day].dinner ? (
-              <input
-                type="text"
-                placeholder="Añadir cena"
-                onBlur={(e) => {
-                  addMeal(day, 'dinner', e.target.value);
-                }}
-              />
-            ) : (
-              <div>
-                <p>{meals[day].dinner}</p>
-                <input 
-                  type="checkbox" 
-                  checked={!inputVisible[day].dinner}
-                  onChange={() => removeMeal(day, 'dinner')}
+              ) : (
+                <div>
+                  <p>{lunch}</p>
+                  <input 
+                    type="checkbox" 
+                    checked={!lunchVisible}
+                    onChange={() => removeMeal(day, 'lunch')}
+                  />
+                </div>
+              )}
+            </div>
+            <div className="meal">
+              <h4>Cena</h4>
+              {dinnerVisible ? (
+                <input
+                  type="text"
+                  placeholder="Añadir cena"
+                  onBlur={(e) => addMeal(day, 'dinner', e.target.value)}
                 />
-              </div>
-            )}
+              ) : (
+                <div>
+                  <p>{dinner}</p>
+                  <input 
+                    type="checkbox" 
+                    checked={!dinnerVisible}
+                    onChange={() => removeMeal(day, 'dinner')}
+                  />
+                </div>
+              )}
+            </div>
           </div>
-        </div>
-      ))}
+        );
+      })}
     </div>
   );
 };
